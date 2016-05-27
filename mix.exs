3a6a967e3b358275761261ef9cd814abbfaa62dd
Omer Wazir <omer@thewazir.com>
2016-05-27 13:06:45 -0700

    Add guardian and dev key

--- mix.exs
+++ mix.exs
@@ -35,7 +35,9 @@ defmodule Peepchat.Mixfile do
      {:phoenix_ecto, "~> 2.0"},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     {:cors_plug, "~> 1.1.2"}]
+     {:cors_plug, "~> 1.1.2"},
+     {:guardian, "~> 0.11.1"}]
+
   end
 
   # Aliases are shortcut or tasks specific to the current project.
