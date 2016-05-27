7d1eb0dd230e72696eb77782a8075de585194c07
Omer Wazir <omer@thewazir.com>
2016-05-27 12:10:49 -0700

    comment out cache thing

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,8 @@ config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "hidden-reaches-29659.herokuapp.com ", port: 443],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+# cache_static_manifest: "priv/static/manifest.json"
 
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
