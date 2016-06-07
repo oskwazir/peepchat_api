5e8ee0d54c515073a49b532ef34e0b6c814dfbef
Omer Wazir <omer@thewazir.com>
2016-06-07 10:37:01 -0700

    Create controller and views for registration

--- web/controllers/registration_controller.ex
+++ web/controllers/registration_controller.ex
@@ -1,12 +1,26 @@
 defmodule Peepchat.RegistrationController do
   use Peepchat.Web, :controller
-
-  def create(conn, %{"data" => %{"type" => "user,"
+  alias Peepchat.User
+  
+  def create(conn, %{"data" => %{"type" => "user",
     "attributes" => %{"email" => email,
       "password" => password,
       "password_confirmation" => password_confirmation}}}) do
-   #Return some static JSON for now
-   conn
-   |> json(%{status:"Okay"})
+    
+    changeset = User.changeset %User{}, %{email: email,
+        password_confirmation: password_confirmation,
+        password: password}
+      
+      case Repo.insert changeset do
+        {:ok, user} ->
+          conn
+          |> put_status(:created)
+          |> render(Peepchat.UserView, "show.json", user: user)
+        {:error, changeset} ->
+          conn
+          |> put_status(:unprocessable_entity)
+          |> render(Peepchat.ChangesetView, "error.json", changeset: changeset)
+      end
+
   end
 end
